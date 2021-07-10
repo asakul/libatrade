3065547ce4db39d254ddc3047d890db61da99fd6
Denis Tereshkin <denis@kasan.ws>
2021-07-10 12:14:25 +0700

    tweak(tests): disable logging

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,21 @@
 
-import qualified TestTypes
 import qualified TestBrokerClient
 import qualified TestBrokerProtocol
 import qualified TestBrokerServer
 import qualified TestQuoteSourceClient
 import qualified TestQuoteSourceServer
+import qualified TestTypes
 import qualified TestZMQTradeSink
 
-import Test.Tasty
+import           Test.Tasty
+
+import           System.Log.Logger
 
 main :: IO ()
-main = defaultMain $ testGroup "Tests" [properties, unitTests]
+main = do
+  logger <- getRootLogger
+  updateGlobalLogger "" (setLevel EMERGENCY)
+  defaultMain $ testGroup "Tests" [properties, unitTests]
 
 properties :: TestTree
 properties = testGroup "Properties" [TestTypes.properties, TestBrokerProtocol.properties]
